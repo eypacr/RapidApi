8ecc7011b0c45ac3df8939fa66072005f2794e99
Acar <Acar@ACAR>
2024-12-02 11:18:17 +0300

    Refactor

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20241202081548_remove_colum_Booking.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    public partial class remove_colum_Booking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Bookings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Frontend/HotelProject.WebUI/Controllers/BookingController.cs
+++ Frontend/HotelProject.WebUI/Controllers/BookingController.cs
@@ -28,7 +28,7 @@ namespace HotelProject.WebUI.Controllers
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(createBookingDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            await client.PostAsync("https://localhost:7298/api/Booking", stringContent);
+            await client.PostAsync("https://localhost:7298/api/Book", stringContent);
             return RedirectToAction("Index", "Default");
         }
     }
